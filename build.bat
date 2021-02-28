e29169f5d85cb10a69d9d518782eb1ca1fca6499
donomii <donomii@gmail.com>
2021-02-28 14:11:20 +0100

    Build in correct directory now

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-mkdir working
-mkdir working\quonverter
-quon.exe compiler.qon   > working\quon.c
-gcc -O3 -Wl,--stack=999999999 working\quon.c -o quon_new
+mkdir work
+mkdir work\quonverter
+quon.exe compiler.qon   > work\quon.c
+gcc -O3 -Wl,--stack=999999999 work\quon.c -o quon_new
 quon_new.exe --test
 quon_new compiler.qon --perl  > work\test.pl
 perl test.pl --test
