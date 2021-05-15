a4369e7991a9ab4df0136e11a3894eae35baff27
donomii <donomii@gmail.com>
2021-05-15 23:38:05 +0200

    Run the correct test builds

--- build.bat
+++ build.bat
@@ -4,9 +4,9 @@ quon.exe compiler.qon   > work\quon.c
 gcc -O3 -Wl,--stack=999999999 work\quon.c -o quon_new
 quon_new.exe --test
 quon_new compiler.qon --perl  > work\test.pl
-perl test.pl --test
+perl work\test.pl --test
 quon_new compiler.qon --node  > work\test.js
-node --stack-size=6000 test.js --test
+node --stack-size=60000 work\test.js --test
 mkdir quonverter
 quon_new compiler.qon --java  > work\quonverter\test.java
 cd work
