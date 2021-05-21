b3da6e4c73cab5d2436ec51862880ade6046876b
Jeremy Price <jeremy.price@grafana.com>
2021-05-21 23:51:19 +0200

    Ansi3 mostly working

--- build.bash
+++ build.bash
@@ -1,20 +1,22 @@
 #!/bin/bash
-./quon compiler.qon   > working/quon.c
+mkdir build
+./quon compiler.qon > working/quon.c
+rm build/quon_new
 gcc -O2 -flto   working/quon.c -Wl,-stack_size,4000000 -o build/quon_new
 build/quon_new --test
-build/quon_new compiler.qon
+#build/quon_new compiler.qon
 
 
 
-mkdir working
-build/quon_new compiler.qon --java > working/test.java
-cd working
-mkdir quonverter
-java -Xss100M -cp . quonverter/MyProgram --test
-javac -d quonverter test.java
-jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
-java -Xss100M -jar MyProgram.jar --test
-cd ..
+#mkdir working
+#build/quon_new compiler.qon --java > working/test.java
+#cd working
+#mkdir quonverter
+#java -Xss100M -cp . quonverter/MyProgram --test
+#javac -d quonverter test.java
+#jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
+#java -Xss100M -jar MyProgram.jar --test
+#cd ..
 
 
 #./quon_new examples/mandelbrot.qon --java > test.java
@@ -25,5 +27,5 @@ cd ..
 #jar -cvfm mandelbrot.jar MANIFEST.MF quonverter/*.class
 #java -Xss100M -jar mandelbrot.jar --test
 
-./quon_new compiler.qon --lua > test.lua
-lua test.lua  compiler.qon --ast
+#./build/quon_new compiler.qon --lua > test.lua
+#lua build/test.lua  compiler.qon --ast
