2e08be1838cc1876a06979a6ae1037a0581454f0
donomii <donomii@gmail.com>
2021-05-22 00:56:30 +0200

    buildwin

--- buildwin.bat
+++ buildwin.bat
@@ -1,6 +1,6 @@
-quon compiler.qon  > quon.c
+quon compiler.qon --ansi3 > quon.c
+rm quon_new.exe
 gcc -O3  -Wl,--stack,167108864  quon.c -o quon_new -g
 quon_new --test
-REM quon_new compiler.qon
 quon_new compiler.qon --perl > test.pl
 perl test.pl --test
