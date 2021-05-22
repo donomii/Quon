5b459e572c758de265bf02a6023015a152b38c1c
donomii <donomii@gmail.com>
2021-05-22 12:57:45 +0200

    Add global releaseMode

--- buildwin.bat
+++ buildwin.bat
@@ -1,6 +1,9 @@
-quon compiler.qon --ansi3 > quon.c
+mkdir working
+quon compiler.qon --ansi3 --release > quon.c
 rm quon_new.exe
 gcc -O3  -Wl,--stack,167108864  quon.c -o quon_new -g
 quon_new --test
-quon_new compiler.qon --perl > test.pl
-perl test.pl --test
+quon_new compiler.qon --perl > working\quon.pl
+perl working\quon.pl --test
+quon_new compiler.qon --node > working\quon.js
+node working\quon.js --test
